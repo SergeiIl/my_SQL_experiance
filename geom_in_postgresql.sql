36a098271cac19d4f21aaa60277e40d54e5e7680
Sergei Il <63243592+SergeiIl@users.noreply.github.com>
2022-06-29 08:03:41 +0300

    Update geom_in_postgresql.sql

--- geom_in_postgresql.sql
+++ geom_in_postgresql.sql
@@ -1,5 +1,8 @@
-SELECT CAST(nonearth.building.id AS int) AS building_id, 
-	   nonearth.gran_distr.name AS na_rayon 
-FROM nonearth.building 
+SELECT nonearth.building.*, 
+	   nonearth.gran_distr.name AS na_rayon,
+	   nonearth.gran_adm_okr.name AS na_ao
+FROM nonearth.building
 LEFT JOIN nonearth.gran_distr 
-	   ON st_within("building"."SP_GEOMETRY", "gran_distr"."SP_GEOMETRY")
+	   ON ST_Intersects("building"."SP_GEOMETRY", "gran_distr"."SP_GEOMETRY")
+LEFT JOIN nonearth.gran_adm_okr 
+	   ON ST_Within("building"."SP_GEOMETRY", "gran_adm_okr"."SP_GEOMETRY")
